5f3f84f8435af16680060f0b44eb4a4977565e73
Carlos Santana <usernam@live.com>
2023-07-28 09:22:59 -0400

    edit stochastic RSI to fit a 21/13/8/8 pattern

--- strategy_files/rsi_reversal.py
+++ strategy_files/rsi_reversal.py
@@ -8,8 +8,22 @@ days_to_backtest = 1
 def rsi_reversal(df):
     symbols = df.columns.get_level_values(0).unique().sort_values(ascending=True)
     rsi = pd.DataFrame()
+    rsi_params = {
+        'length': 13,
+        'rsi_length': 21,
+        'k': 8,
+        'd': 8,
+    }
+    rsi_column_name = (
+        f'STOCHRSId'
+        f'_{rsi_params["length"]}'
+        f'_{rsi_params["rsi_length"]}'
+        f'_{rsi_params["k"]}'
+        f'_{rsi_params["d"]}'
+    )
+
     for symbol in symbols:
-        rsi_single = df[symbol].ta.stochrsi()
+        rsi_single = df[symbol].ta.stochrsi(**rsi_params)
         rsi_single['Ticker'] = symbol
         rsi_single = rsi_single.set_index('Ticker', append=True).unstack('Ticker').swaplevel(axis=1)
         rsi = pd.concat([rsi, rsi_single], axis=1)
@@ -17,7 +31,7 @@ def rsi_reversal(df):
     close = df.loc[:, df.columns.get_level_values(1).isin(['Close'])].droplevel(1, axis='columns')
     high = df.loc[:, df.columns.get_level_values(1).isin(['High'])].droplevel(1, axis='columns')
     low = df.loc[:, df.columns.get_level_values(1).isin(['Low'])].droplevel(1, axis='columns')
-    rsi_14 = rsi.loc[:, rsi.columns.get_level_values(1).isin(['STOCHRSId_14_14_3_3'])].droplevel(1, axis='columns')
+    rsi_14 = rsi.loc[:, rsi.columns.get_level_values(1).isin([rsi_column_name])].droplevel(1, axis='columns')
     
     gt_0day_1day = rsi_14 > rsi_14.shift(1)
     lt_0day_1day = rsi_14 < rsi_14.shift(1)
